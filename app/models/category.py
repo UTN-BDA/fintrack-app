6bf63ea821fb1465f11e97d1363c57554610e3d8
Juan Geyer <juanjogeyer@gmail.com>
2025-05-08 20:56:35 -0300

    Add models

---
+++ app/models/category.py
@@ -0,0 +1,10 @@
+from app import db
+
+class Category(db.Model):
+    __tablename__ = 'category'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    is_favorite = db.Column(db.Boolean, default=False)
+    is_recurring = db.Column(db.Boolean, default=False)
+    transactions = db.relationship('Transaction', back_populates='category', lazy='dynamic')
---
+++ app/models/transaction.py
@@ -0,0 +1,18 @@
+from app import db
+
+class Transaction(db.Model):
+    __tablename__ = 'transaction'
+
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Numeric(10, 2), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    description = db.Column(db.String(255))
+    method = db.Column(db.String(50))
+    is_income = db.Column(db.Boolean, nullable=False, default=False)
+    deleted = db.Column(db.Boolean, default=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+
+    user = db.relationship('User', back_populates='transactions')
+    category = db.relationship('Category', back_populates='transactions')
