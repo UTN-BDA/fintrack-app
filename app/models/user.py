b14a6598a4fb18526d292124d301949bb797f656
juan1lopez <juanilpz86@gmail.com>
2025-05-15 21:37:07 -0300

    Edit the model user and update the migrations

--- app/models/user.py
+++ app/models/user.py
@@ -7,7 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     transactions = db.relationship('Transaction', back_populates='user', lazy='dynamic')
 
     def set_password(self, password: str):
