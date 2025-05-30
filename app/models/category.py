127061c13995cf15ea7f19aaaf74313e9da6934a
juan1lopez <juanilpz86@gmail.com>
2025-05-30 19:35:14 -0300

    Add indexes

--- app/models/category.py
+++ app/models/category.py
@@ -1,8 +1,11 @@
+from sqlalchemy import Index
 from app.extensions import db
 
 class Category(db.Model):
     __tablename__ = 'category'
-
+    __table_args__ = (
+        Index('ix_category_name', 'name'),
+    )
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     is_favorite = db.Column(db.Boolean, default=False)
--- app/models/transaction.py
+++ app/models/transaction.py
@@ -1,7 +1,24 @@
+from sqlalchemy import Index, text
 from app.extensions import db
 
 class Transaction(db.Model):
     __tablename__ = 'transaction'
+    __table_args__ = (
+        # Para listar rápido transacciones de un usuario
+        Index('ix_txn_user', 'user_id'),
+        # Para listar rápido transacciones de un usuario por fecha
+        Index('ix_txn_user_date', 'user_id', 'date'),
+        # Búsquedas por categoría
+        Index('ix_txn_category', 'category_id'),
+        # Si filtras mucho por tipo ingreso/egreso
+        Index('ix_txn_income', 'is_income'),
+        # Para excluir las transacciones borradas de forma ultra‑rápida sin tocar todas las filas (Postgres partial index)
+        Index(
+            'ix_txn_active',
+            'id',
+            postgresql_where=text('deleted = false')
+        ),
+    )
 
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Numeric(10, 2), nullable=False)
