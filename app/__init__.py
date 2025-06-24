4598cd2fa7030cfcf4d98736c585fff04950a51a
Juan Geyer <juanjogeyer@gmail.com>
2025-06-24 20:11:55 -0300

    Add Redis caching configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,33 @@
 from flask import Flask
 from flask_migrate import Migrate
 from app.config.config import config
+from app.config.cache_config import cache_config
 from app.resources.routes import RouteApp
+from flask_caching import Cache
 import os
 from app.extensions import db
 
 migrate = Migrate()
+cache = Cache()
 
 def create_app():
     app_context = os.getenv("FLASK_CONTEXT")
     print(f"app_context: {app_context}")
 
-    app = Flask(__name__)
+    app = Flask(
+        __name__,
+        template_folder="fronted/templates",  # Ruta a la carpeta templates
+        static_folder="fronted/static"       # Ruta a la carpeta static
+    )
+
     configuration = config[app_context if app_context else 'development']
     app.config.from_object(configuration)
+    app.secret_key = os.getenv('SECRET_KEY')
     
     db.init_app(app)
     migrate.init_app(app, db)
+    cache.init_app(app, config=cache_config)
+    
     route = RouteApp()
     route.init_app(app)
 
---
+++ app/config/cache_config.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+"""
+Configuración de la caché para la aplicación.
+Utiliza Redis como backend de caché y carga las credenciales desde variables de entorno.
+"""
+
+basedir = os.path.abspath(Path(__file__).parents[2])
+load_dotenv(os.path.join(basedir, '.env'))
+
+cache_config = {
+    'CACHE_TYPE': 'RedisCache',
+    'CACHE_DEFAULT_TIMEOUT': 300,
+    'CACHE_REDIS_HOST': os.environ.get('REDIS_HOST'),
+    'CACHE_REDIS_PORT': int(os.environ.get('REDIS_PORT')),
+    'CACHE_REDIS_DB': int(os.environ.get('REDIS_DB')),
+    'CACHE_REDIS_PASSWORD': os.environ.get('REDIS_PASSWORD'),
+    'CACHE_KEY_PREFIX': 'flask_'
+}
